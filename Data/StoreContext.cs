5a76e2372033ec3191729944e5343decb00ae077
Diyar Faraj <diyar.faraj@gmail.com>
2022-04-25 14:33:27 +0200

    create db

---
+++ Data/StoreContext.cs
@@ -0,0 +1,14 @@
+﻿using KochiStockholm.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace KochiStockholm.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext( DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
