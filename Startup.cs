c16548e17f7df7cba362121ea94f0aad935cd570
Diyar Faraj <diyar.faraj@gmail.com>
2022-04-27 12:16:13 +0200

    add cors

--- Startup.cs
+++ Startup.cs
@@ -38,6 +38,7 @@ namespace ecommerceApi
             {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +54,11 @@ namespace ecommerceApi
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(opt =>
+            {
+                opt.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:3000");
+            });
 
             app.UseAuthorization();
 
