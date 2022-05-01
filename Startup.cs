caf5a5c9fc27249785ce1582444ca11d93f54813
Diyar Faraj <diyar.faraj@gmail.com>
2022-05-01 17:37:58 +0200

    remove comment

--- Startup.cs
+++ Startup.cs
@@ -48,7 +48,6 @@ namespace ecommerceApi
             app.UseMiddleware<ExceptionMiddleware>();
             if (env.IsDevelopment())
             {
-               // app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "ecommerceApi v1"));
             }
