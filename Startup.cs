618fc77d48c30ecbce9d240bccc62227e6bad47a
Diyar Faraj <diyar.faraj@gmail.com>
2022-09-21 19:58:03 +0200

    configure swagger for bearer

--- Startup.cs
+++ Startup.cs
@@ -40,6 +40,27 @@ namespace ecommerceApi
             services.AddSwaggerGen(c =>
             {   
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "ecommerceApi", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "Jwt auth header",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }
+                    }
+                })
             });
             services.AddDbContext<StoreContext>(opt =>
             {
