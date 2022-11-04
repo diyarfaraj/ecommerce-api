92bf0bbd04f088574198b1be2052602795ea96c3
Diyar Faraj <diyar.faraj@gmail.com>
2022-11-04 21:02:38 +0100

    craete order controller

---
+++ Controllers/OrdersController.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using ecommerceApi.Data;
+using ecommerceApi.Entities.OrderAggregate;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ecommerceApi.Controllers
+{
+    [Authorize]
+    public class OrdersController : BaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public OrdersController(StoreContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Order>> GetOrders()
+        {
+            return await _context.Orders
+                .Include(o => o.OrderItems)
+                .Where(x => x.BuyerId == User.Identity.Name)
+                .ToListAsync();
+        }
+    }
+}
