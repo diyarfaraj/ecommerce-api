2f96a4de18769114f447d1c3f591d8029a58371e
Diyar Faraj <diyar.faraj@gmail.com>
2022-11-08 22:37:58 +0100

    fill in order DTO

--- DTOs/OrderDto.cs
+++ DTOs/OrderDto.cs
@@ -1,6 +1,19 @@
-﻿namespace ecommerceApi.DTOs
+﻿using System;
+using System.Collections.Generic;
+using ecommerceApi.Entities.OrderAggregate;
+
+namespace ecommerceApi.DTOs
 {
     public class OrderDto
     {
+        public int Id { get; set; }
+        public string BuyerId { get; set; }
+        public ShippingAdress ShippingAdress { get; set; }
+        public DateTime OrderDate { get; set; }
+        public List<OrderItemDto> OrderItems { get; set; }
+        public long SubTotal { get; set; }
+        public long DeliveryFee { get; set; }
+        public string OrderStatus { get; set; }
+        public long Total { get; set; }
     }
 }
