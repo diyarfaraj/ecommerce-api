6f813fcb29bfefeab29677bd9c0e91785131b0ac
Diyar Faraj <diyar.faraj@gmail.com>
2022-11-13 22:40:45 +0100

    create extension method

---
+++ Extensions/OrderExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+using ecommerceApi.DTOs;
+using ecommerceApi.Entities.OrderAggregate;
+
+namespace ecommerceApi.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IQueryable<OrderDto> ProjectOrderToOrderDto( this IQueryable<Order> query)
+        {
+            //map them props
+        }
+    }
+}
