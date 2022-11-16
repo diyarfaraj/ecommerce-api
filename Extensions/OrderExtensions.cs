41e2e7de15cd86c66e763dc7fa7256a55e96e1f8
Diyar Faraj <diyar.faraj@gmail.com>
2022-11-17 00:58:50 +0100

    working on order extension

--- Extensions/OrderExtensions.cs
+++ Extensions/OrderExtensions.cs
@@ -9,6 +9,14 @@ namespace ecommerceApi.Extensions
         public static IQueryable<OrderDto> ProjectOrderToOrderDto( this IQueryable<Order> query)
         {
             //map them props
+            return query
+                .Select(order => new OrderDto
+                {
+                    Id = order.Id,
+                    BuyerId = order.BuyerId,
+                    OrderDate = order.OrderDate,
+                    OrderStatus = 
+                });
         }
     }
 }
