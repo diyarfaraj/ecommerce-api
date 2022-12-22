5708286972dbf31dc35095f26fcbf591cfb4be4d
Diyar Faraj <diyar.faraj@gmail.com>
2022-12-22 14:54:07 +0100

    added payment service

--- Startup.cs
+++ Startup.cs
@@ -93,6 +93,7 @@ namespace ecommerceApi
 
             services.AddAuthorization();
             services.AddScoped<TokenService>();
+            services.AddScoped<PaymentService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
