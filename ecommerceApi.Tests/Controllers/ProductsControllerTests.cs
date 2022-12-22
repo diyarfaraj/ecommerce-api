9d72bb61b3bb8343c0bcd257b088377e7e3a8748
Diyar Faraj <diyar.faraj@gmail.com>
2022-12-22 21:38:43 +0100

    fixed tests for buggy controller

---
+++ ecommerceApi.Tests/Controllers/ProductsControllerTests.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ecommerceApi.Controllers;
+using ecommerceApi.Data;
+using ecommerceApi.Entities;
+using ecommerceApi.RequestHelpers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Xunit;
+
+namespace ecommerceApi.Tests
+{
+    public class ProductsControllerTests
+    {
+        
+        
+    }
+}
