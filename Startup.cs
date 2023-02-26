dfb14bd284a6244f4e88bb741f40dd30b3a84a9c
Diyar Faraj <diyar.faraj@gmail.com>
2023-02-26 18:18:57 +0100

    upgrade to .NET 6 done

--- Startup.cs
+++ Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection; 
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
