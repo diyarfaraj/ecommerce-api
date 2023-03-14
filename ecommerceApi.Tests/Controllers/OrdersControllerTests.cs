36b929edab31a9135508597954ace8022b819b97
Diyar Faraj <diyar.faraj@gmail.com>
2023-03-14 20:25:09 +0100

    new tests add

--- ecommerceApi.Tests/Controllers/OrdersControllerTests.cs
+++ ecommerceApi.Tests/Controllers/OrdersControllerTests.cs
@@ -1,74 +1,57 @@
-﻿using ecommerceApi.Controllers;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using ecommerceApi.Data;
 using ecommerceApi.DTOs;
-using ecommerceApi.Entities;
 using ecommerceApi.Entities.OrderAggregate;
-using FakeItEasy;
+using ecommerceApi.Extensions;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
 using Xunit;
+using FakeItEasy;
+using ecommerceApi.Controllers;
+using ecommerceApi.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
-namespace ecommerceApi.Tests.Controllers
+namespace ecommerceApi.Tests
 {
     public class OrdersControllerTests
     {
+        private readonly IdentityDbContext<User, Role, int> _context;
+        private readonly OrdersController _controller;
+
+        public OrdersControllerTests()
+        {
+            _context = A.Fake<IdentityDbContext<User, Role, int>>();
+            A.CallTo(() => _context.Set<Order>()).Returns(new List<Order>
+        {
+            new Order { Id = 1 },
+            new Order { Id = 2 }
+        }.AsQueryable());
+            // Configure other DbSets here
+
+            _controller = new OrdersController(_context);
+        }
         [Fact]
-        public async Task GetOrders_ReturnsExpectedListOfOrdersForUser()
+        public async Task GetOrders_ReturnsListOfOrderDtos()
         {
             // Arrange
-            var context = A.Fake<StoreContext>();
-            var controller = new OrdersController(context);
-            var orders = new List<Order>()
+            var orders = new List<OrderDto>
             {
-                new Order() { Id = 1, BuyerId = "user1" },
-                new Order() { Id = 2, BuyerId = "user1" },
-                new Order() { Id = 3, BuyerId = "user2" }
+                new OrderDto { Id = 1 },
+                new OrderDto { Id = 2 }
             };
-           // A.CallTo(() => context.Orders).ReturnsDbSet(orders);
+
+            A.CallTo(() => _context.Orders.ProjectOrderToOrderDto())
+                .Returns(orders.AsQueryable());
 
             // Act
-            var result = await controller.GetOrders();
-            var okResult = result.Result as OkObjectResult;
-            var orderDtos = okResult.Value as List<OrderDto>;
+            var result = await _controller.GetOrders();
 
             // Assert
-            Assert.NotNull(orderDtos);
-            Assert.Equal(2, orderDtos.Count);
-            Assert.True(orderDtos.All(o => o.BuyerId == "user1"));
+            var actionResult = Assert.IsType<ActionResult<List<OrderDto>>>(result);
+            var model = Assert.IsAssignableFrom<List<OrderDto>>(actionResult.Value);
+            Assert.Equal(2, model.Count);
         }
-
-        //[Fact]
-        //public async Task CreateOrder_CreatesOrderAndReturnsCreatedOrderId()
-        //{
-        //    // Arrange
-        //    var context = A.Fake<StoreContext>();
-        //    var controller = new OrdersController(context);
-        //    var basket = new Basket() { BuyerId = "user1", PaymentIntentId = "pi_123" };
-        //    var productItem = new Product() { Id = 1, Name = "Product 1", ImgUrl = "https://example.com/product1.jpg", Price = 100, QuantityInStock = 5 };
-        //    var basketItem = new BasketItem() { ProductId = productItem.Id, Quantity = 2 };
-        //    basket.Items.Add(basketItem);
-        //    //A.CallTo(() => context.Baskets).ReturnsDbSet(new List<Basket>() { basket });
-        //    A.CallTo(() => context.Products.FindAsync(productItem.Id)).Returns(productItem);
-        //    //A.CallTo(() => context.SaveChangesAsync()).Returns(1);
-
-        //    // Act
-        //    var orderDto = new CreateOrderDto()
-        //    {
-        //        ShippingAdress = new ShippingAdress() { FullName = "John Doe", Address1 = "123 Main St", City = "New York", Country = "USA", Zip = "10001", State = "NY" },
-        //        SaveAddress = true
-        //    };
-        //    var result = await controller.CreateOrder(orderDto);
-        //    var createdAtRouteResult = result.Result as CreatedAtRouteResult;
-
-        //    // Assert
-        //    Assert.NotNull(createdAtRouteResult);
-        //    Assert.Equal("GetOrder", createdAtRouteResult.RouteName);
-        //    //Assert.Equal(basket.PaymentIntentId, (createdAtRouteResult.RouteValues["id"] as int?).Value);
-        //}
     }
-
 }
